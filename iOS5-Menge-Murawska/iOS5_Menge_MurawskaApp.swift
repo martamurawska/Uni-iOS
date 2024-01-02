ad899b8e78c127693bccf17e356abb715b5dbd2a
martamurawska <marta.murawska55@gmail.com>
2024-01-02 10:06:41 +0100

    Initial Commit

---
+++ iOS5-Menge-Murawska/iOS5_Menge_MurawskaApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS5_Menge_MurawskaApp.swift
+//  iOS5-Menge-Murawska
+//
+//  Created by Marta Murawska on 02.01.24.
+//
+
+import SwiftUI
+
+@main
+struct iOS5_Menge_MurawskaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOS5-Menge-MurawskaUITests/iOS5_Menge_MurawskaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  iOS5_Menge_MurawskaUITestsLaunchTests.swift
+//  iOS5-Menge-MurawskaUITests
+//
+//  Created by Marta Murawska on 02.01.24.
+//
+
+import XCTest
+
+final class iOS5_Menge_MurawskaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
